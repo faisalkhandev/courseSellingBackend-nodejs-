92847074ee1d3e4bd8da98558d209596cb1b5829
faisalKhan <faisaldev404@gmail.com>
2025-04-22 23:32:33 +0500

    refactor user route for improved signup handling and validation

--- index.js
+++ index.js
@@ -5,28 +5,25 @@ const { userRouter } = require("./routes/userRoute");
 const { courseRouter } = require("./routes/courseRoute");
 const { adminRouter } = require("./routes/adminRoute");
 
+
 const app = express();
 
+app.use(express.json());
 
 // loads the env variables
 dotenv.config();
 
-connectDB()
+// connecting Database
+connectDB();
 
 
+// Prefix Routes
 app.use("/api/v1/user", userRouter)
 app.use("/api/v1/course", courseRouter)
 app.use("/api/v1/courseCreator", adminRouter)
 
 
-
-
-
-
-
-
-
-
+//PORT
 const PORT = process.env.PORT || 3002;
 
 app.listen(PORT, () => {
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,13 +1,40 @@
-const { Router } = require("express")
+const { Router } = require("express");
+const { userModel } = require("../models/allModel");
 
 const userRouter = Router();
 
 
-userRouter.post("/signup", async function (req, res) {
-    res.json({
-        message: "sign up sucessfully."
-    })
-})
+userRouter.post("/signup", async (req, res) => {
+
+
+    try {
+        const { email, firstname, lastname, password } = req.body;
+
+        if (!email || !firstname || !lastname || !password) {
+            return res.json({
+                message: "Enter the requried fields."
+            })
+        }
+
+
+        const newUser = await userModel.create({
+            email,
+            firstname,
+            lastname,
+            password,
+        });
+
+        res.status(201).json({
+            message: "User created successfully.",
+            user: newUser,
+        });
+    } catch (err) {
+        console.error(err);
+
+        res.status(500).json({ message: "Something went wrong." });
+    }
+});
+
 userRouter.post("/signin", async function (req, res) {
     res.json({
         message: "sign up sucessfully."
